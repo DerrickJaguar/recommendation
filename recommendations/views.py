6cc581b9af598d5c90e1b65addc9f2b89e6bb249
DerrickJaguar <derrric65@gmail.com>
2024-10-10 10:12:46 +0300

    Initial commit of recomendation library

---
+++ recommendations/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+# recommendation/views.py
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .engine.hybrid import HybridRecommendationEngine
+from .serializers import TrackSerializer, RecommendationSerializer
+
+class TrackRecommendationsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        engine = HybridRecommendationEngine()
+        recommended_tracks = engine.recommend(request.user)
+        serializer = TrackSerializer(recommended_tracks, many=True)
+        return Response(serializer.data)
+
+class UserPreferencesView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        engine = HybridRecommendationEngine()
+        preferences = engine.get_user_preferences(request.user)
+        serializer = RecommendationSerializer(preferences)
+        return Response(serializer.data)
